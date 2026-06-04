9f052e4b979325edd2addd94b9276fed21680b3b
Christna Yosua Rotinsulu <yosuamic123@gmail.com>
2026-06-04 12:00:23 +0700

    fix: update frontend submodule and add missing notification service

--- fetch_sonar.py
+++
@@ -1,5 +0,0 @@
-import urllib.request, json
-url = 'https://sonarcloud.io/api/issues/search?componentKeys=advprog-2026-A15-project_yomu-api-gateway'
-data = json.loads(urllib.request.urlopen(url).read())
-for i in data['issues']:
-    print(f"[{i['type']}] {i['rule']} in {i['component']} at line {i.get('line', 'N/A')}: {i['message']}")
